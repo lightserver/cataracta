a31b26fb0a13bb02c2342dc13e3fdc83c72149dd
Jarek Ratajski <jratajski@gmail.com>
2015-12-22 16:49:24 +0100

    deleted domain data

--- app/shared/src/main/scala/pl/setblack/mb137/data/BoardMessage.scala
+++
@@ -1,6 +0,0 @@
-package pl.setblack.mb137.data
-
-case class BoardMessage(val author: String,
-                        val txt: String,
-                        timestamp : Long,
-                         uuid : String = "07")
